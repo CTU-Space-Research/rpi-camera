5305bc0d35a4200c5ca1998751666fe1e7abea6b
Jiri Kvapil <kvapiji2@fit.cvut.cz>
2025-10-03 16:48:55 +0200

    fix github path

--- preinstall.sh
+++ preinstall.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install git ffmpeg rpicam-apps -y
 
 cd -
-git clone https://github.com/CTU-Space-Research/rpi-camera-service.git camera
+git clone https://github.com/CTU-Space-Research/rpi-camera.git camera
 cd camera
 sudo ./install.sh
