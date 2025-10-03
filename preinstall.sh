202d54a879899c481557552c1b3c7e2849c5e27a
Jirka Kvapil <53862157+kvapca@users.noreply.github.com>
2025-10-03 16:40:30 +0200

    Add commands to clone and install rpi-camera-service

--- preinstall.sh
+++ preinstall.sh
@@ -2,6 +2,7 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install git ffmpeg rpicam-apps
 
+cd -
 git clone https://github.com/CTU-Space-Research/rpi-camera-service.git camera
 cd camera
 sudo ./install.sh
