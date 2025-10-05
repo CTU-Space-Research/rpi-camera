87d2ea864991190f689f232f87a3514995672b0e
Jirka Kvapil <53862157+kvapca@users.noreply.github.com>
2025-10-05 21:33:32 +0200

    Change directory to home before cloning repository

--- preinstall.sh
+++ preinstall.sh
@@ -4,7 +4,7 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install git ffmpeg rpicam-apps -y
 
-cd -
+cd ~
 git clone https://github.com/CTU-Space-Research/rpi-camera.git camera
 cd camera
 sudo ./install.sh
