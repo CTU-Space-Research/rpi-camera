f3418acd5b117f801b991bf26a28773a3519eae2
Jiri Kvapil <kvapiji2@fit.cvut.cz>
2025-10-07 12:04:51 +0200

    Add installation progress messages to preinstall script

--- preinstall.sh
+++ preinstall.sh
@@ -4,7 +4,10 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install git ffmpeg rpicam-apps -y
 
+echo -e "\nInstalling rpi-camera"
+echo "${PWD}"
 cd ~
+echo "${PWD}"
 git clone https://github.com/CTU-Space-Research/rpi-camera.git camera
 cd camera
 sudo ./install.sh
